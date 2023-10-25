4298731ba6133cc1f91519f4e31aebf9140364c2
NassKillers <mr.nedjar.nassim@gmail.com>
2023-10-25 14:00:59 +0200

    add csvData variable for new value csv

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -10,6 +10,7 @@ function Input({ setCsvData, csvData, setHeaders, setLoading }) {
             const lines = csvText.split('\n');
             const headers = lines[0].split(',');
     
+            const csvData = [];
             for (let i = 1; i < lines.length; i++) {
                 const dataLine = lines[i].split(',');
                 const rowData = {};
@@ -31,8 +32,10 @@ function Input({ setCsvData, csvData, setHeaders, setLoading }) {
     return (
         <div className="App">
             <h1>Mesures de pression de l'air</h1>
-            <label htmlFor="file">file</label>
-            <input type="file" accept=".csv" id="file" onChange={handleFile} />
+            <div className="input" style={{ display: 'flex', flexDirection: 'column' }}>
+                <label htmlFor="file">Pick a file</label>
+                <input type="file" accept=".csv" id="file" onChange={handleFile} />
+            </div>
         </div>
     )
 }
