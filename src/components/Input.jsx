936c91903c821479f26226cc40c54b71a68c28d4
NassKillers <mr.nedjar.nassim@gmail.com>
2023-10-25 13:49:03 +0200

    create input component

---
+++ src/components/Input.jsx
@@ -0,0 +1,40 @@
+function Input({ setCsvData, csvData, setHeaders, setLoading }) {
+
+    const handleFile = (e) => {
+        setLoading(true);
+        const file = e.target.files[0];
+        const reader = new FileReader();
+    
+        reader.onload = (e) => {
+            const csvText = e.target.result;
+            const lines = csvText.split('\n');
+            const headers = lines[0].split(',');
+    
+            for (let i = 1; i < lines.length; i++) {
+                const dataLine = lines[i].split(',');
+                const rowData = {};
+                for (let j = 0; j < headers.length; j++) {
+                rowData[headers[j]] = dataLine[j];
+                }
+                csvData.push(rowData);
+            }
+    
+            setCsvData(csvData);
+            setHeaders(headers);
+
+            setLoading(false);
+        };
+    
+        reader.readAsText(file);
+      };
+
+    return (
+        <div className="App">
+            <h1>Mesures de pression de l'air</h1>
+            <label htmlFor="file">file</label>
+            <input type="file" accept=".csv" id="file" onChange={handleFile} />
+        </div>
+    )
+}
+
+export default Input;
